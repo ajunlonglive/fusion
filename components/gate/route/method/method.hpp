804c96db2733e2c9b85e9727e6a072d6c6e99487
onecthree <onecthree@gmail.com>
2022-08-03 20:06:04 +0700

    re-short code in "Method" method for FRouter

--- components/gate/route/method/method.hpp
+++ components/gate/route/method/method.hpp
@@ -14,10 +14,6 @@
 #include <vector>
 #include <functional>
 
-int testf() {
-    return 0;
-}
-
 class RouteMethod : public Php::Base {
 
     // constructor
@@ -30,22 +26,20 @@ class RouteMethod : public Php::Base {
 
         std::string request_method = Database::get::string({"FUSION_STORE", "FS_ROUTE", "REQUEST_METHOD"});
 
-        std::map<std::string, std::function<int()>> method_list_hitted;
-        method_list_hitted["GET"] = std::bind(&RouteMethod::get_method, this);
-        method_list_hitted["POST"] = std::bind(&RouteMethod::post_method, this);;
-        method_list_hitted["PUT"] = std::bind(&RouteMethod::put_method, this);;
-        method_list_hitted["PATCH"] = std::bind(&RouteMethod::patch_method, this);;
-        method_list_hitted["DELETE"] = std::bind(&RouteMethod::delete_method, this);;
-        method_list_hitted["OPTIONS"] = std::bind(&RouteMethod::options_method, this);;
-
         bool is_method_valid = false;
         for(auto &each_method : (Php::Value)param[0]) {
             std::string method_temp = (const char *) each_method.second;
-            std::transform(method_temp.begin(), method_temp.end(),method_temp.begin(), ::toupper);
+            std::transform(method_temp.begin(), method_temp.end(), method_temp.begin(), ::toupper);
+
 
             if(request_method == method_temp) {
-                method_list_hitted[request_method]();
+                // Override request method in database, for trigger the actually requested method
+                override_request_method(request_method);
+                
+                // Set is_method_valid to true for change the conditional
                 is_method_valid = true;
+                
+                // Break the loop process
                 break;
             }
         }
@@ -63,9 +57,6 @@ class RouteMethod : public Php::Base {
                 std::string request_uri = Database::get::string({"FUSION_STORE", "FS_ROUTE", "FS_REQUEST_URI"});
 
                 if(SmartRouter::handle_input_uri_guard(escape_uri_route) || uri_route == request_uri) {
-                    // Change PUT_METHOD parameter to "true" for tell to Router Service, if routing request method given
-                    // Database::set::string({"FUSION_STORE", "FS_ROUTE", "OPTIONS_METHOD", "is_null"}, "false");
-
                     // Assign request context to Router Services
                     RouteService::web::assign(escape_uri_route, handler_opt, "METHOD");
                 }
@@ -73,11 +64,8 @@ class RouteMethod : public Php::Base {
         }
     }
 
-    public: int get_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "GET_METHOD", "is_null"}, "false"); return 0; }
-    public: int post_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "POST_METHOD", "is_null"}, "false"); return 0; }
-    public: int put_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "PUT_METHOD", "is_null"}, "false"); return 0; }
-    public: int patch_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "PATCH_METHOD", "is_null"}, "false"); return 0; }
-    public: int delete_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "DELETE_METHOD", "is_null"}, "false"); return 0; }
-    public: int options_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "OPTIONS_METHOD", "is_null"}, "false"); return 0; }
-    public: int head_method() { Database::set::string({"FUSION_STORE", "FS_ROUTE", "HEAD_METHOD", "is_null"}, "false"); return 0; }
+    public: void override_request_method(std::string REQUEST_METHOD) {
+        Database::set::string({"FUSION_STORE", "FS_ROUTE", REQUEST_METHOD+ "_METHOD", "is_null"}, "false");
+    }
+
 };
