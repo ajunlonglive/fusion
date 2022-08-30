17b78551c13e86763cd77c8257aafb313290a4bf
onecthree <onecthree@gmail.com>
2022-08-30 10:43:48 +0700

    testing up more keyword and dynamic reserved token

--- tests/compiler.cpp
+++ tests/compiler.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 
 int main() {
@@ -11,18 +12,18 @@ int main() {
     std::string content( (std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()) );
     // std::cout << content[0] << std::endl;
     std::vector<std::string> keyword = {
-        "%php ",
+        "%php",
         " %ephp",
 
         "{{",
         "}}",
 
-        "%if ", 
-        "%else ",
-        "%eif ",
+        "%if", 
+        "%else",
+        "%eif",
 
-        "%for ",
-        "%efor ",
+        "%for",
+        "%efor",
     };
     std::vector<std::string> act_keyword = {
         "<?php ",
@@ -30,30 +31,69 @@ int main() {
 
         "<?php echo ",
         " ?>",
-        "<?php if ",
+
+        "<?php if", // %if
         "<?php else: ?>",
         "<?php endif; ?>",
 
-        "<?php for ",
+        "<?php for",
         "<?php } ?>",
     };
+
     std::unordered_map<std::string, std::string> phar_keyword;
                             phar_keyword["%if"] = "): ?>";
                             phar_keyword["%for"] = ") { ?>";
-                            
+
+    std::vector<std::string> dynamic_keyword = {"%for"};
+
+    std::vector<std::string> token_dynamic_keyword = {
+        " as ",
+    };
+
+    std::unordered_map<std::string, std::string> pattern_dynamic_keyword;
+                            pattern_dynamic_keyword[" as "] = "each";
+
 
     int content_length = 0;
+
     std::string compiled_content = "";
+    std::string temp_compiled_content = "";
 
     int open_phar_tracer = 0;
+
     std::string current_phar_keyword = "";
 
     std::string find_phar = "false";
 
-    int looped_count = 0;
+    bool current_find_dynamic_keyword = false;
 
     for(std::size_t index_content = 0; index_content < content.size(); ++index_content) {
         if(index_content < content_length) continue;
+
+        if(current_find_dynamic_keyword) {
+            for(std::size_t index_token_dynamic_keyword = 0; index_token_dynamic_keyword < token_dynamic_keyword.size(); ++index_token_dynamic_keyword) {
+                int index_token_dynamic_keyword_length = 0;
+
+                for(std::size_t index_char_token_dynamic_keyword = 0; index_char_token_dynamic_keyword < token_dynamic_keyword[index_token_dynamic_keyword].size(); ++index_char_token_dynamic_keyword) {
+                    if(token_dynamic_keyword[index_token_dynamic_keyword][index_char_token_dynamic_keyword] == content[content_length + index_char_token_dynamic_keyword]) {
+                        index_token_dynamic_keyword_length++;
+                    } else {
+                        index_token_dynamic_keyword_length = 0;
+                        break;
+                    }
+                }
+
+                if(index_token_dynamic_keyword_length > 0) {
+                    // content_length += index_token_dynamic_keyword_length;
+                    compiled_content += pattern_dynamic_keyword[token_dynamic_keyword[index_token_dynamic_keyword]];
+                    compiled_content += temp_compiled_content;
+                    temp_compiled_content = "";
+                    current_find_dynamic_keyword = false;
+
+                    break;
+                }
+            }
+        }
 
         if(content[content_length] == '(' && find_phar == "true") {
             open_phar_tracer++;
@@ -68,7 +108,11 @@ int main() {
             std::string act_phar_keyword = phar_keyword[current_phar_keyword];
             // content_length += act_phar_keyword.length();
             content_length++;
-            compiled_content += phar_keyword[current_phar_keyword];
+            if(current_find_dynamic_keyword) {
+                temp_compiled_content += phar_keyword[current_phar_keyword];
+            } else {
+                compiled_content += phar_keyword[current_phar_keyword];
+            }
             find_phar = "false";
         }
 
@@ -76,43 +120,59 @@ int main() {
         for(std::size_t index_keyword = 0; index_keyword < keyword.size(); ++index_keyword) { // per keyword e.g. {{, }}, if, else
 
             int token_length = 0;
-
-            // per char keyword e.g. {, { -- }, } -- i, f -- e, l, s,  e
+            
+            // per char keyword e.g. ({, {) -- (}, }) -- i, f -- e, l, s,  e
             for(std::size_t index_char_keyword = 0; index_char_keyword < keyword[index_keyword].size(); ++index_char_keyword) {
-                // std::cout << keyword[index_keyword][index_char_keyword] << " == " << content[content_length + index_char_keyword] << std::endl;
+
 
                 if(keyword[index_keyword][index_char_keyword] == content[content_length + index_char_keyword]){
                     token_length++;
-
-                    std::cout << "len token : " << token_length << std::endl;
                 } else {
                     token_length = 0;
                     break;
                 }
+
             }
 
             if(token_length > 0) {
                 content_length += token_length;
-                // std::cout << "keyword keganti : \"" << act_keyword[index_keyword] << "\" -- " << index_keyword << std::endl;
-                compiled_content += act_keyword[index_keyword];
+                if(current_find_dynamic_keyword) {
+                    temp_compiled_content += act_keyword[index_keyword];
+                } else {
+                    compiled_content += act_keyword[index_keyword];
+                }
 
                 if(phar_keyword.find(keyword[index_keyword]) != phar_keyword.end()) {
+
                     current_phar_keyword = keyword[index_keyword];
                     find_phar = "true";
+
+                    if (std::find(dynamic_keyword.begin(), dynamic_keyword.end(), keyword[index_keyword]) != dynamic_keyword.end()) {
+                        current_find_dynamic_keyword = true;
+                    }
                 }
 
                 break;
             }   
 
-            // looped_count++;
-            // std::cout << "IS LOOPED : " << looped_count << std::endl;
         }
 
         if(index_content < content_length) continue;
+
         content_length++;
-        compiled_content += content[index_content];
+
+        if(current_find_dynamic_keyword) {
+            temp_compiled_content += content[index_content];
+        } else {
+            compiled_content += content[index_content];
+        }
+
+        // compiled_content += temp_compiled_content;
     }
 
+    // std::cout << compiled_content << std::endl;
+    // std::cout << temp_compiled_content << std::endl;
+    compiled_content += temp_compiled_content;
     std::cout << compiled_content << std::endl;
 
     return 0;
